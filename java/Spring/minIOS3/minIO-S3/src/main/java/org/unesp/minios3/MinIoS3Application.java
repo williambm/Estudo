0c92717998ca2573829b9a21c7d211958bcfbdf4
William Bragagnollo Montini <william.montini@unesp.br>
2025-03-13 10:34:49 -0300

    Estudo de miniIO-S3 com spring

---
+++ java/Spring/minIOS3/minIO-S3/src/main/java/org/unesp/minios3/MinIoS3Application.java
@@ -0,0 +1,13 @@
+package org.unesp.minios3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinIoS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinIoS3Application.class, args);
+	}
+
+}
---
+++ java/Spring/minIOS3/minIO-S3/src/test/java/org/unesp/minios3/MinIoS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unesp.minios3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinIoS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
