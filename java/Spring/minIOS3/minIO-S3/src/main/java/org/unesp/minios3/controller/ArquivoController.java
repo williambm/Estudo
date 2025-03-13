63f45f22db2640354cac631f2c97dcb6e53b4c64
William Bragagnollo Montini <william.montini@unesp.br>
2025-03-13 15:55:39 -0300

    endpoint de download wip

--- java/Spring/minIOS3/minIO-S3/src/main/java/org/unesp/minios3/controller/ArquivoController.java
+++ java/Spring/minIOS3/minIO-S3/src/main/java/org/unesp/minios3/controller/ArquivoController.java
@@ -1,9 +1,62 @@
 package org.unesp.minios3.controller;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.unesp.minios3.service.ArquivoService;
+
+import java.awt.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/arquivos")
 public class ArquivoController {
+
+    private final ArquivoService minioService;
+
+    public ArquivoController(ArquivoService minioService) {
+        this.minioService = minioService;
+    }
+
+    /**
+     * Upload de arquivo
+     */
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        String fileUrl = minioService.uploadFile(file);
+        return ResponseEntity.status(HttpStatus.CREATED).body(fileUrl);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<String>> listaArquivosDoBucket() {
+        //Listar todos os arquivos do bucket, como no service tem o nome do bucket ele traz tudo pelo nome.
+        List<String> fileUrls = minioService.listFiles();
+        return ResponseEntity.ok(fileUrls);
+    }
+
+    /**
+     * Dicas:
+     *
+     * HttpHeaders.CONTENT_DISPOSITION → Define que o conteúdo será um download de arquivo.
+     * "attachment; filename=\"" + fileName + "\"" → Formata corretamente o nome do arquivo.
+     *
+     * A busca por nome de arquivo deve ser realizada com o nome exato!!!
+     */
+    @GetMapping("/download")
+    public ResponseEntity<Resource> downloadFile(@RequestParam String fileName, @RequestParam String contentType) {
+        Resource arquivoRecuperado = minioService.downloadFile(fileName);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .contentType(MediaType.parseMediaType(contentType))
+                .body(arquivoRecuperado);
+    }
 }
--- java/Spring/minIOS3/minIO-S3/src/main/java/org/unesp/minios3/service/ArquivoService.java
+++ java/Spring/minIOS3/minIO-S3/src/main/java/org/unesp/minios3/service/ArquivoService.java
@@ -1,8 +1,32 @@
 package org.unesp.minios3.service;
 
+import io.minio.ListObjectsArgs;
 import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.Result;
+import io.minio.errors.ErrorResponseException;
+import io.minio.errors.InsufficientDataException;
+import io.minio.errors.InternalException;
+import io.minio.errors.InvalidResponseException;
+import io.minio.errors.ServerException;
+import io.minio.errors.XmlParserException;
+import io.minio.messages.Item;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class ArquivoService {
@@ -14,5 +38,92 @@ public class ArquivoService {
 
     public ArquivoService(MinioClient minioClient) {
         this.minioClient = minioClient;
+    }
+
+    /**
+     * Upload de arquivo para o MinIO.
+     * <p>
+     * Para fazer o upload de um arquivo para o MinIO, é necessário criar um objeto PutObjectArgs. Esse objeto já possui um builder
+     * que auxilia nessa criação do upload.
+     */
+    public String uploadFile(MultipartFile file) {
+        try {
+            String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+            //Mapa Opcional de metadados - Nomes devem ser sem espaço e minúsculos aparentemente
+            Map<String, String> metadata = Map.of(
+                    "nome_original", file.getOriginalFilename(),
+                    "tag_sisproec", "Bolsista"
+            );
+
+            //Try With Resources - aqui ele vai fechar o InputStream automaticamente, mesmo que ocorra uma exceção.
+            try (InputStream inputStream = file.getInputStream()) {
+                minioClient.putObject(
+                        PutObjectArgs.builder()
+                                .bucket(bucketName)
+                                .object(fileName)
+                                .stream(inputStream, file.getSize(), -1)
+                                .contentType(file.getContentType())
+                                .userMetadata(metadata) //Opcionalmente, podemos passar metadados para o arquivo
+                                .build()
+                );
+            }
+
+            //Aqui estou retornando o caminho completo do arquivo, incluindo o nome do bucket.Mas poderia ser um DTO personalizado
+            return "/" + bucketName + "/" + fileName;
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao fazer upload para MinIO: " + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Lista todos os arquivos no bucket.
+     * Documentação: https://min.io/docs/minio/linux/developers/java/API.html#listObjects
+     */
+    public List<String> listFiles() {
+        try {
+            // A listagem dos arquivos do Bucket Retorna um Iterable<Result<Item>> Result e Item são do Minio
+            Iterable<Result<Item>> resultados = minioClient.listObjects(
+                    ListObjectsArgs.builder()
+                            .bucket(bucketName)
+                            .build()
+            );
+            List<String> nomesArquivos = new ArrayList<>();
+            // Iterando sobre os resultados e adicionando os nomes dos arquivos à lista, mas poderia ser um DTO personalizado com mais informações
+            resultados.forEach(itemResult -> {
+                try {
+                    nomesArquivos.add(itemResult.get().objectName());
+                } catch (ErrorResponseException |
+                         InsufficientDataException |
+                         InternalException |
+                         InvalidKeyException |
+                         InvalidResponseException |
+                         IOException |
+                         NoSuchAlgorithmException |
+                         ServerException |
+                         XmlParserException e) {
+                    throw new RuntimeException(e.getMessage());
+                }
+            });
+
+            return nomesArquivos;
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao listar arquivos: " + e.getMessage(), e);
+        }
+    }
+
+    public Resource downloadFile(String fileName) {
+        //InputStream não pode ser usado em um try with Resources, senão ele é fechado antes de ser usado!
+        try {
+            InputStream stream = minioClient.getObject(
+                    io.minio.GetObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(fileName)
+                            .build());
+            //Converte o InputStream em um Resource, que é o tipo que o Spring Boot entende como um recurso.
+            return new InputStreamResource(stream);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Arquivo não encontrado: " + fileName);
+        }
     }
 }
